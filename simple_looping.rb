9ab46ab37deffc567b9dd0c62b102714836aa8f6
Meghan Campbell <mlc03e@gmail.com>
2018-10-12 19:12:50 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,8 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  loop do 
    counter = 1
+  loop do 
    puts "Welcome to Flatiron School's Web Development Course!"
    break if counter == number_of_times
    counter = counter + 1
